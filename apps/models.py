2fd9d8bcc8bfce9f89721c8b289b86541e1560ba
itsjasminn <muvvahhida@gmail.com>
2025-12-10 09:41:09 +0500

    updated

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+from django.db.models import Model, ForeignKey, CASCADE, TextField, DateTimeField
+from django.forms import ImageField
 
-# Create your models here.
+
+class Post(Model):
+    author = ForeignKey('auth_.User', on_delete=CASCADE, related_name='posts')
+    content = TextField()
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
+
+
+class PostImage(Model):
+    post = ForeignKey('auth_.Post', on_delete=CASCADE, related_name='images')
+    image = ImageField()
+
+
+class PostView(Model):
+    class Meta:
+        unique_together = ('post', 'user')
+
+    post = ForeignKey('auth_.Post', on_delete=CASCADE, related_name='views')
+    user = ForeignKey('auth_.User', on_delete=CASCADE, related_name='views')
+
+
+class Comment(Model):
+    post = ForeignKey('auth_.Post', on_delete=CASCADE, related_name='comments')
+    author = ForeignKey('auth_.User', on_delete=CASCADE, related_name='comments')
+    content = TextField()
+    created_at = DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author} commented on {self.post}"
+
+
+class Like(Model):
+    post = ForeignKey('auth_.Post', on_delete=CASCADE, related_name='likes')
+    user = ForeignKey('auth_.User', on_delete=CASCADE, related_name='likes')
+    created_at = DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} liked {self.post}"
