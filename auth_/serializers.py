ec267342ee14c3f04e0cace28067ef57e02da792
itsjasminn <muvvahhida@gmail.com>
2025-12-11 17:42:35 +0500

    updated

--- auth_/serializers.py
+++ auth_/serializers.py
@@ -1,12 +1,19 @@
+import json
+
 from django.contrib.auth.hashers import make_password
-from django.core.validators import validate_email
+from django.core.validators import validate_email, RegexValidator
 from rest_framework.exceptions import ValidationError
-from rest_framework.serializers import ModelSerializer
+from rest_framework.fields import CharField
+from rest_framework.serializers import ModelSerializer, Serializer
 
 from auth_.models import User
+from root.settings import redis
 
 
 class UserModelSerializer(ModelSerializer):
+    referral_code = CharField(read_only=True)
+    referred_by_code = CharField(write_only=True, required=False)
+
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password', 'avatar', 'bio',)
@@ -23,11 +30,42 @@ class UserModelSerializer(ModelSerializer):
 
         return value
 
+    username_validation = RegexValidator(
+        regex=r'^[a-zA-Z0-9_.-]+$',
+        message="Username should contain only letters, numbers and underscores."
+    )
+
+    def validate_username(self, value):
+        self.username_validation(value)
+
+        reserved = ['admin', 'user', 'root', 'null']
+
+        if len(value) < 3:
+            raise ValidationError('Username must be at least 3 characters long.')
+        if value.lower() in reserved:
+            raise ValidationError('This username is not valid!')
+        if User.objects.filter(username=value).exists():
+            raise ValidationError('This username is already taken!')
+
+        return value
+
     def validate_password(self, value):
         if len(value) < 4:
             raise ValidationError('Password must be at least 4 characters!')
 
         return make_password(value)
+
+
+class VerifyCodeSerializer(Serializer):
+    code = CharField(max_length=6)
+
+    def validate_code(self, value):
+        data = redis.get(value)
+        if not data:
+            raise ValidationError('Invalid code!')
+        user_data = json.loads(data)
+        self.context['user_data'] = user_data
+        return value
 
 
 class UserUpdateSerializer(ModelSerializer):
